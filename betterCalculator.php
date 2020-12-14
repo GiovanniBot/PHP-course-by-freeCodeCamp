6ceb3b2d5f2c92b6544b6809ef26980190cda048
GiovanniBot <gioalmeida07@gmail.com>
2020-12-14 13:58:46 -0300

    Decimals numbers bug fixed

--- betterCalculator.php
+++ betterCalculator.php
@@ -5,11 +5,11 @@
     <title>Four Operations Calculator</title>
   </head>
   <body>
-    <h1>Fou Operations Calculator</h1>
+    <h1>Four Operations Calculator</h1>
     <form action="betterCalculator.php" method="post">
-        First Number:<br><input type="number" name="num1"><br>
+        First Number:<br><input type="number" step="0.1" name="num1"><br>
         OP: <br><input type="text" name="op"><br>
-        Second Number:<br><input type="number" name="num2"><br>
+        Second Number:<br><input type="number" step="0.1" name="num2"><br>
         <input type="submit">
     </form>
     <?php
