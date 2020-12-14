07b70fbef728b1f46bb3c85ff993c49e6ba1e60c
GiovanniBot <gioalmeida07@gmail.com>
2020-12-14 05:15:02 -0300

    Four Operations Calculator

---
+++ betterCalculator.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Four Operations Calculator</title>
+  </head>
+  <body>
+    <h1>Fou Operations Calculator</h1>
+    <form action="betterCalculator.php" method="post">
+        First Number:<br><input type="number" name="num1"><br>
+        OP: <br><input type="text" name="op"><br>
+        Second Number:<br><input type="number" name="num2"><br>
+        <input type="submit">
+    </form>
+    <?php
+      $num1 = $_POST["num1"];
+      $num2 = $_POST["num2"];
+      $op = $_POST["op"];
+      if($op == "+"){
+        echo $num1 + $num2;
+      } elseif ($op == "-"){
+        echo $num1 - $num2;
+      } elseif ($op == "*"){
+        echo $num1 * $num2;
+      } elseif ($op == "/"){
+        echo $num1 / $num2;
+      } else{
+        echo "Not a valid operator (+, -, *, /).";
+      }
+    ?>
+  </body>
+</html>
