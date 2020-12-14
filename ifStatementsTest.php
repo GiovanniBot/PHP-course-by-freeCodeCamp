2567e7ec9acf1b72eb6a023bacad34d91d4cf8a0
GiovanniBot <gioalmeida07@gmail.com>
2020-12-14 01:27:46 -0300

    If Statements Test

--- ifStatementsTest.php
+++ ifStatementsTest.php
@@ -7,27 +7,40 @@
 </head>
 <body>
   <h3>Which roles do you play?</h3>
-  <form action="ifStatementsTest.php" method="post"> 
+  <form action="ifStatementsTest.php" method="post">
     Top laner: <input type="checkbox" name="role[]" value="top";><br>
     Jungler: <input type="checkbox" name="role[]" value="jg";><br>
     Mid laner: <input type="checkbox" name="role[]" value="mid";><br>
     ADC: <input type="checkbox" name="role[]" value="adc";><br>
     Support: <input type="checkbox" name="role[]" value="sup";><br>
-    <input type="submit"> 
+    <input type="submit">
   </form>
   <?php
     $playerRole = array($_POST['role']);
     print_r($playerRole);
-    if ($playerRole === 'top'){
+    if ($playerRole === 'top') {
       echo "You can test Darius, it's a nice start to Top lane.";
+    } elseif ($playerRole === 'jg') {
+      echo "Try Rammus and be free to run all the map in 3 seconds.";
+    } elseif ($playerRole === 'mid') {
+      echo "You can test Annie, it's a good way to burn your enemies patience.";
+    } elseif ($playerRole === 'adc') {
+      echo "ADC in 2020... nonono"
+    } else {
+      echo "Support is everything.";
     }
-    else ($playerRole === 'jg'){
+
+
+
+
+
+     ($playerRole === 'jg'){
       echo "Try Rammus and be free to run all the map in 3 seconds.";
     }
     else ($playerRole === 'mid'){
       echo "You can test Annie, it's a good way to burn your enemies patience.";
     }
-    
+
   ?>
 </body>
 </html>
