c8e6a5859c194d5b96ea5f176713959c73e08d5a
GiovanniBot <gioalmeida07@gmail.com>
2020-12-14 13:57:03 -0300

    Learning Foreach

--- ifStatementsTest.php
+++ ifStatementsTest.php
@@ -3,31 +3,32 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>If Statements Test</title>
+  <title>If Statements Test(I ended up learning foreach)</title>
 </head>
 <body>
   <h3>Which roles do you play?</h3>
   <form action="ifStatementsTest.php" method="post">
-    Top laner: <input type="checkbox" name="role[]" value="top";><br>
+    Top lane: <input type="checkbox" name="role[]" value="top";><br>
     Jungler: <input type="checkbox" name="role[]" value="jg";><br>
-    Mid laner: <input type="checkbox" name="role[]" value="mid";><br>
+    Mid lane: <input type="checkbox" name="role[]" value="mid";><br>
     ADC: <input type="checkbox" name="role[]" value="adc";><br>
     Support: <input type="checkbox" name="role[]" value="sup";><br>
     <input type="submit">
-  </form>
+  </form><br>
   <?php
-    $playerRole = array($_POST['role']);
-    print_r($playerRole);
-    if ($playerRole === top) {
-      echo "You can test Darius, it's a nice start to Top lane.";
-    } elseif ($playerRole === jg) {
-      echo "Try Rammus and be free to run all the map in 3 seconds.";
-    } elseif ($playerRole === mid) {
-      echo "You can test Annie, it's a good way to burn your enemies patience.";
-    } elseif ($playerRole === adc) {
-      echo "ADC in 2020... nonono";
-    } else {
-      echo "Support is everything.";
+    $_POST['role'];
+    foreach($_POST['role'] as $value){
+      if($value == "top"){
+        echo "For Top lane you can test Darius, it's a nice start. ";
+      } elseif ($value == "jg"){
+        echo "In Jungle, try Rammus and be free to run all the map in 3 seconds. ";
+      } elseif ($value == "mid"){
+        echo "For Mid lane you can test Annie, it's a good way to burn your enemies patience. ";
+      } elseif ($value == "adc"){
+        echo "ADC in 2020... nonono. ";
+      } else {
+        echo "Support is everything. ";
+      }
     }
   ?>
 </body>
