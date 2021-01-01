e7230c0b7358d0b8604ec966fcf04d86cfd62320
GiovanniBot <gioalmeida07@gmail.com>
2021-01-01 08:03:36 -0300

    While Loop

---
+++ whileLoop.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>while Loops</title>
+  </head>
+  <body>
+    <?php
+      $index = 1;
+      while($index <= 5){
+        echo "$index <br>";
+        $index++;
+      }
+    ?>
+  </body>
+</html>
