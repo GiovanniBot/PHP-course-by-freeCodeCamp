cafe305fba965efd472573f8d70c101d79241199
GiovanniBot <gioalmeida07@gmail.com>
2021-01-01 07:52:50 -0300

    Switch Statements Title

--- switchStatements.php
+++ switchStatements.php
@@ -2,7 +2,7 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Switch Statements</title>
   </head>
   <body>
     <form action="switchStatements.php" method="post">
